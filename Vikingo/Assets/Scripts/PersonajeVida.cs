6d65b780d01a3300b0d9a9b26f994c6bc9ad337a
Brayan Steven León Martinez <brot10102017@gmail.com>
2024-08-03 16:43:48 -0500

    Brayan - Actualización de Salud Personaje

---
+++ Vikingo/Assets/Scripts/PersonajeVida.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PersonajeVida : VidaBase
+{
+    public bool PuedeSerCurado => Salud < saludMax;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            RecibirDaño(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestaurarVida(10);
+        }
+    }
+
+    public void RestaurarVida(float cantidad)
+    {
+        if (PuedeSerCurado)
+        {
+            Salud += cantidad;
+            if(Salud > saludMax)
+            {
+                Salud = saludMax;
+            }
+
+            ActualizarBarraVida(Salud, saludMax);
+        }
+    }
+
+    protected override void ActualizarBarraVida(float vidaActual, float vidaMax)
+    {
+        
+    }
+}
