7fae2ee8e6e2751f6620073645686ec25669d757
Brayan Steven León Martinez <brot10102017@gmail.com>
2024-08-28 19:45:50 -0500

    Brayan - Añadir Item Inventario

---
+++ Vikingo/Assets/Scripts/Inventario/ItemPorAgregar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPorAgregar : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private InventarioItem inventarioItemReferencia;
+    [SerializeField] private int cantidadPorAgregar;
+
+}
