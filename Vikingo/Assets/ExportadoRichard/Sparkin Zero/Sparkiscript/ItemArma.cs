dbf5fa17ab49afd9d905824c9cb033e22e5aa4af
Brayan Steven León Martinez <124888972+bleon133@users.noreply.github.com>
2024-10-12 19:42:10 -0500

    Diego

---
+++ Vikingo/Assets/ExportadoRichard/Sparkin Zero/Sparkiscript/ItemArma.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Arma")]
+public class ItemArma : InventarioItem
+{
+    [Header("Arma")]
+    public Arma Arma;
+}
