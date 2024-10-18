97f653fa59c27db320757637aa7e262bd9e22422
cokis8685 <ruedastiven852@gmail.com>
2024-10-18 17:22:33 -0500

    Trigger

--- Vikingo/Assets/Scripts/Interfaz/Trigger.cs
+++ Vikingo/Assets/Scripts/Interfaz/Trigger.cs
@@ -9,24 +9,9 @@ public class Trigger : MonoBehaviour
 
     [SerializeField] private UnityEvent EventoEntrar;
     [SerializeField] private UnityEvent EventoSalir;
-
-    [SerializeField] private GameObject A_D_objeto;
-
     [SerializeField] private bool ActivadoInicial = false;
 
     private bool AdentrodelTrigger = false;
-
-    private void Start()
-    {
-        if (A_D_objeto != null)
-        {
-            A_D_objeto.SetActive(ActivadoInicial);
-        }
-        else
-        {
-            Debug.LogError("Falta asignar un objeto en A_D_objeto.");
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -34,18 +19,6 @@ public class Trigger : MonoBehaviour
         {
             AdentrodelTrigger = true;
             EventoEntrar?.Invoke();
-
-            // Intentamos obtener el componente PersonajeVida
-            PersonajeVida personajeVida = other.GetComponent<PersonajeVida>();
-            if (personajeVida != null)
-            {
-                personajeVida.RestaurarVida(10); // Regenerar 10 puntos de vida al entrar
-                Debug.Log("Vida restaurada al personaje.");
-            }
-            else
-            {
-                Debug.LogWarning("El objeto que entró en el trigger no tiene el componente PersonajeVida.");
-            }
         }
     }
 
@@ -55,21 +28,6 @@ public class Trigger : MonoBehaviour
         {
             AdentrodelTrigger = false;
             EventoSalir?.Invoke();
-        }
-    }
-
-    private void Update()
-    {
-        if (AdentrodelTrigger && Input.GetKeyDown(KeyCode.F))
-        {
-            if (A_D_objeto != null)
-            {
-                A_D_objeto.SetActive(!A_D_objeto.activeSelf);
-            }
-            else
-            {
-                Debug.LogError("Falta asignar un objeto en A_D_objeto.");
-            }
         }
     }
 }
