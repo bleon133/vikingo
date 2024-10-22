87a1e27bcaa7a89e4d2f95c18424bff9399c156b
Brayan Steven León Martinez <124888972+bleon133@users.noreply.github.com>
2024-10-21 21:04:50 -0500

    Brayan - update

--- Vikingo/Assets/Scripts/Triggers/PersonajeController.cs
+++ Vikingo/Assets/Scripts/Triggers/PersonajeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PersonajeController : MonoBehaviour
 {
@@ -15,9 +16,38 @@ public class PersonajeController : MonoBehaviour
         }
         else
         {
-            // Asigna la instancia actual y marca para no destruirla al cambiar de escena
+            // Asigna la instancia actual
             instancia = this;
-            DontDestroyOnLoad(this.gameObject);
+
+            // Verifica el nombre de la escena
+            string escenaActual = SceneManager.GetActiveScene().name;
+
+            // No aplicar DontDestroyOnLoad si es una escena específica
+            if (escenaActual != "Menu" && escenaActual != "Cinematica")
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        // Suscribirse al evento de cambio de escena
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        // Desuscribirse del evento de cambio de escena
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Si se ha cargado una escena de "Menu" o "Cinematica", destruir el objeto
+        if (scene.name == "Menu" || scene.name == "Cinematica")
+        {
+            Destroy(this.gameObject);
         }
     }
 }
